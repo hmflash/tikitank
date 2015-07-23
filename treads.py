ece5aa474a824eb2dfee8199e176baf1cbc33ad5
Seth Hinze <ultrix@gmail.com>
2015-07-22 18:54:33 -0700

    Move calibration into class and out of main.

--- treads.py
+++ treads.py
@@ -5,21 +5,44 @@ import subprocess, os.path
 import sys, time
 
 class SpeedSensor(adc.Capture):
-	def __init__(self, *k, **kw):
+	def __init__(self, firmware, *k, **kw):
 		super(SpeedSensor, self).__init__(*k, **kw)
 
-	def start(self, firmware_src):
-		fw_bin = os.path.splitext(firmware_src)[0]
-		cmd = [ 'pasm', '-V3', '-b', firmware_src, fw_bin ]
+		fw = os.path.splitext(firmware)[0]
+		cmd = [ 'pasm', '-V3', '-b', firmware, fw ]
 		p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 		(stdout, stderr) = p.communicate()
 
 		if p.returncode != 0:
 			print stdout
-			raise IOError('Failed to compile "%s"' % firmware_src)
+			raise IOError('Failed to compile "%s"' % firmware)
 
-		fw = os.path.abspath(fw_bin + '.bin')
-		adc._pru_adc.Capture.start(self, fw)
+		self._firmware = fw = os.path.abspath(fw + '.bin')
+
+		self.encoder0_pin = kw.get('pin', 0)
+
+	def calibrate(self):
+		print 'Running encoder for 10 seconds...'
+
+		self.encoder0_threshold = 4096
+		self.encoder0_delay = 0
+		adc._pru_adc.Capture.start(self, self._firmware)
+
+		time.sleep(10)
+		timer = s.timer
+		_, min0, max0, _, _ = s.encoder0_values
+
+		self.close()
+
+		print 'Capture runs at %d readings per second' % (timer // 10)
+		print 'Time value of one timer unit is %d nanosec' % (1000000000 // timer)
+		print 'Range for the encoder:', min0, '-', max0
+		print 'Recommended threshold value for encoder is:', int(0.9*(max0-min0))
+
+	def start(self):
+		s.encoder0_threshold = 750
+		s.encoder0_delay = 100
+		adc._pru_adc.Capture.start(self, self._firmware)
 
 	def close(self):
 		self.stop()
@@ -27,42 +50,26 @@ class SpeedSensor(adc.Capture):
 		super(SpeedSensor, self).close()
 
 if __name__ == '__main__':
-	s = SpeedSensor()
+	s = SpeedSensor('tank/firmware.p')
 
-	if len(sys.argv) > 1:
-		s.encoder0_threshold = 4096
-		s.start('tank/firmware.p')
-
-		time.sleep(10)
-		timer = s.timer
-		_, min0, max0, _, _ = s.encoder0_values
-		s.close()
-
-		print 'Capture runs at %d readings per second' % (timer // 10)
-		print 'Time value of one timer unit is %d nanosec' % (1000000000 // timer)
-		print 'Range for the encoder:', min0, '-', max0
-		print 'Recommended threshold value for encoder is:', int(0.9*(max0-min0))
+	if '--test' in sys.argv:
+		s.calibrate()
 		sys.exit()
-	else:
-		s.encoder0_threshold = 750
-		s.encoder0_delay = 100
-		s.start('tank/firmware.p')
-
-	num = 0
-	last = s.encoder0_ticks
 
 	try:
+		s.start()
+
+		if '-v' in sys.argv:
+			while True:
+				print s.timer, s.encoder0_values
+
+		num = 0
+
 		while num < 10:
-			#print s.timer, s.encoder0_values
-			#continue
-
-			val = s.encoder0_ticks
-
-			if last != val:
-				delta = val - last
-				num += delta
+			delta = s.encoder0_ticks - num
+			if delta:
 				print 'Tick: %s' % delta
-				last = val
+				num += delta
 
 	except KeyboardInterrupt:
 		s.close()
