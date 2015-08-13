7734390f67e261f35f8d8c10629513f09b6704db
Frank Laub <frank.laub@gmail.com>
2015-08-12 21:17:14 -0700

    Bresenham to the rescue

--- static/view.js
+++ static/view.js
@@ -1,82 +1,86 @@
 "use strict";
 
-// $(document).ready(function () {
-// 	var canvas = document.getElementById('canvas');
-// 	var width = 250;
-// 	var height = 250;
-// 	var ctx = canvas.getContext('2d');
-
-// 	var img = ctx.createImageData(width, height);
-
-// 	var ws = new WebSocket('ws://' + location.host);
-// 	ws.binaryType = 'arraybuffer';
-	
-// 	ws.onopen = function(event) {
-// 		console.log('onopen');
-// 	};
-
-// 	ws.onmessage = function(event) {
-// 		var view = new Uint8ClampedArray(event.data);
-// 		for (var i = 0; i < view.byteLength / 3; i++) {
-// 			img.data[i*4+0] = view[i*3+1];
-// 			img.data[i*4+1] = view[i*3+0];
-// 			img.data[i*4+2] = view[i*3+2];
-// 			img.data[i*4+3] = 0xff;
-// 		}
-	
-// 		ctx.putImageData(img, 0, 0);
-// 	}
-// });
-
 $(document).ready(function () {
 	var canvas = document.getElementById('canvas');
-	var width = 480;
-	var height = 1;
+	var width = 500;
+	var height = 500;
 	var ctx = canvas.getContext('2d');
-
-	var pcanvas = document.createElement('canvas');
-	pcanvas.width = width;
-	pcanvas.height = height;
-	var pctx = pcanvas.getContext('2d');
-	var img = pctx.createImageData(width, height);
+	var img = ctx.createImageData(width, height);
 
 	var ws = new WebSocket('ws://' + location.host);
 	ws.binaryType = 'arraybuffer';
 	
-	ws.onopen = function(event) {
-		console.log('onopen');
-	};
-
 	ws.onmessage = function(event) {
-		var view = new Uint8ClampedArray(event.data);
-		for (var i = 0; i < view.byteLength / 3; i++) {
-			img.data[i*4+0] = view[i*3+1];
-			img.data[i*4+1] = view[i*3+0];
-			img.data[i*4+2] = view[i*3+2];
-			img.data[i*4+3] = 0xff;
-		}
-		pctx.putImageData(img, 0, 0);
+		var colors = new Uint8ClampedArray(event.data);
 
-		var pattern = ctx.createPattern(pcanvas, 'repeat');
+		var g = {
+			img: img,
+			colors: colors,
+			i: 0
+		};
 
-		var origin = [10, 10];
+		var origin = [0, 0];
+		var offset = [20, 20];
 
 		var vertices = [
-			[0, 10],
-			[50, 25],
-			[250, 25],
-			[265, 15],
-			[265, 5],
-			[250, 0],
-			[0, 0]		
+			[0   , 10 ],
+			[50  , 25 ],
+			[250 , 25 ],
+			[265 , 15 ],
+			[265 , 5  ],
+			[250 , 0  ],
+			[0   , 0  ]		
 		];
 
-		ctx.beginPath();
-		ctx.strokeStyle = pattern;
-		ctx.moveTo(origin[0], origin[1]);
+		var prev = origin;
 		for (var i = 0; i < vertices.length; i++) {
-			ctx.lineTo(vertices[i][0] + origin[0], vertices[i][1] + origin[1]);
+			var cur = vertices[i];
+			line(g,
+				prev[0]+offset[0], prev[1]+offset[1], 
+				cur[0]+offset[0], cur[1]+offset[1]
+			);
+			prev = cur;
 		}
-		ctx.stroke();
+
+		ctx.putImageData(img, 0, 0);
 	}
 });
+
+function plot(g, x, y) {
+	var j = y*g.img.width*4 + x*4;
+	var i = g.i % g.colors.byteLength;
+
+	g.img.data[j+0] = g.colors[i+0] & 0x7f * 2;  // R
+	g.img.data[j+1] = g.colors[i+1] & 0x7f * 2;  // G
+	g.img.data[j+2] = g.colors[i+2] & 0x7f * 2;  // B
+	g.img.data[j+3] = 0xff;                      // A
+
+	g.i += 3;
+}
+
+function line(g, x0, y0, x1, y1) {
+	var dx = Math.abs(x1 - x0);
+	var sx = x0 < x1 ? 1 : -1;
+	var dy = Math.abs(y1 - y0);
+	var sy = y0 < y1 ? 1 : -1;
+	var err = (dx > dy ? dx : -dy)/2;
+
+	while (true) {
+		plot(g, x0, y0);
+
+		if (x0 === x1 && y0 === y1)
+			break;
+
+		var e2 = err;
+
+		if (e2 > -dx) {
+			err -= dy;
+			x0 += sx;
+		}
+
+		if (e2 < dy) {
+			err += dx;
+			y0 += sy;
+		}
+	}
+}
